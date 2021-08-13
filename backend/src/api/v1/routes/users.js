0a0453b1f2bf6ee03d2bc36ee4fc8ba9bc1515a8
Samdish <samdeeshsingh001@gmail.com>
2021-08-13 19:39:34 +0530

    Add route for registering user and use registerUser controller function

--- backend/src/api/v1/routes/users.js
+++ backend/src/api/v1/routes/users.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const { protect, admin } = require('../middlewares');
-const { getMyAccount, getUser } = require('../controllers').users;
+const {
+  getMyAccount,
+  getUser,
+  registerUser,
+} = require('../controllers').users;
 
 router.route('/me').get(protect, getMyAccount);
 router.route('/user/:id').get(protect, admin, getUser);
+router.post('/register', registerUser);
 
 module.exports = router;
