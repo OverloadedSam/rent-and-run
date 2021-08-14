4ab5865b5eaf89d03b100cde8f07440bb8ac590b
Samdish <samdeeshsingh001@gmail.com>
2021-08-14 18:24:12 +0530

    Add route for updating user account information and use updateUser controller function

--- backend/src/api/v1/routes/users.js
+++ backend/src/api/v1/routes/users.js
@@ -4,9 +4,10 @@ const {
   getMyAccount,
   getUser,
   registerUser,
+  updateUser,
 } = require('../controllers').users;
 
-router.route('/me').get(protect, getMyAccount);
+router.route('/me').get(protect, getMyAccount).put(protect, updateUser);
 router.route('/user/:id').get(protect, admin, getUser);
 router.post('/register', registerUser);
 
