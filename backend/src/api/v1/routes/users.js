7ca8e17ea585ef08acc8cd7befd506929571532b
Samdish <samdeeshsingh001@gmail.com>
2021-08-14 18:27:34 +0530

    Add route for user login and use loginUser controller function

--- backend/src/api/v1/routes/users.js
+++ backend/src/api/v1/routes/users.js
@@ -5,10 +5,12 @@ const {
   getUser,
   registerUser,
   updateUser,
+  loginUser,
 } = require('../controllers').users;
 
 router.route('/me').get(protect, getMyAccount).put(protect, updateUser);
 router.route('/user/:id').get(protect, admin, getUser);
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;
