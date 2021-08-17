61029158546f7d619ad103c5376e28832ecc5ac9
Samdish <samdeeshsingh001@gmail.com>
2021-08-17 19:08:02 +0530

    Add route for updating roles and use updateRole controller function

--- backend/src/api/v1/routes/roles.js
+++ backend/src/api/v1/routes/roles.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const { protect, admin } = require('../middlewares');
-const { getRoles, createRole } = require('../controllers').roles;
+const { getRoles, createRole, updateRole } = require('../controllers').roles;
 
 router
   .get('/roles', protect, admin, getRoles)
-  .post('/createRole', protect, admin, createRole);
+  .post('/createRole', protect, admin, createRole)
+  .put('/updateRole', protect, admin, updateRole);
 
 module.exports = router;
