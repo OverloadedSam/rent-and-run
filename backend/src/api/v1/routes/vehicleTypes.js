826163d13dbdc0a51822a03b389e09be39fe227a
Samdish <samdeeshsingh001@gmail.com>
2021-08-18 18:28:44 +0530

    Add route for creating vehicle types and use createVehicleType controller function

--- backend/src/api/v1/routes/vehicleTypes.js
+++ backend/src/api/v1/routes/vehicleTypes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
-const { getVehicleTypes } = require('../controllers').vehicleTypes;
+const { protect, admin } = require('../middlewares');
+const { getVehicleTypes, createVehicleType } =
+  require('../controllers').vehicleTypes;
 
 router.get('/vehicleTypes', getVehicleTypes);
+router.post('/createVehicleType', protect, admin, createVehicleType);
 
 module.exports = router;
