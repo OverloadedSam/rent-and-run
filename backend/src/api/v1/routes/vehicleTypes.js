daebcbabe690b31c7842cce362f5a01e89f21a97
Samdish <samdeeshsingh001@gmail.com>
2021-08-18 18:41:47 +0530

    Add route for updating vehicle types and use updateVehicleType controller function

--- backend/src/api/v1/routes/vehicleTypes.js
+++ backend/src/api/v1/routes/vehicleTypes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const { protect, admin } = require('../middlewares');
-const { getVehicleTypes, createVehicleType } =
+const { getVehicleTypes, createVehicleType, updateVehicleType } =
   require('../controllers').vehicleTypes;
 
 router.get('/vehicleTypes', getVehicleTypes);
 router.post('/createVehicleType', protect, admin, createVehicleType);
+router.put('/updateVehicleType', protect, admin, updateVehicleType);
 
 module.exports = router;
