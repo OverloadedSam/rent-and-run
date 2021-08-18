692ba27f81e03f707426816454031638c4f4a970
Samdish <samdeeshsingh001@gmail.com>
2021-08-18 18:25:53 +0530

    Vehicle type controller to create vehicle types

--- backend/src/api/v1/controllers/vehicleTypes.js
+++ backend/src/api/v1/controllers/vehicleTypes.js
@@ -18,6 +18,27 @@ const getVehicleTypes = asyncHandler(async (req, res, next) => {
   });
 });
 
+// @route   POST /api/v1/createVehicleType
+// @access  Admin
+// @desc    Create vehicle types for vehicles.
+const createVehicleType = asyncHandler(async (req, res, next) => {
+  const vehicleTypeData = { ...req.body };
+
+  if (!vehicleTypeData.name) {
+    return next(new ErrorResponse(400, 'Vehicle type name is not found!'));
+  }
+
+  const vehicleType = new VehicleType(vehicleTypeData);
+  const [result] = await vehicleType.addVehicleType();
+
+  res.status(201).json({
+    success: true,
+    status: 201,
+    data: { ...vehicleTypeData, id: result.insertId },
+  });
+});
+
 module.exports = {
   getVehicleTypes,
+  createVehicleType,
 };
