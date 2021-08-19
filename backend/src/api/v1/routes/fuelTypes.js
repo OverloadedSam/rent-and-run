0017d4e29c6807d8177211b6233e15415b52249b
Samdish <samdeeshsingh001@gmail.com>
2021-08-19 14:12:41 +0530

    Add route for creating fuel types and use createFuelType controller

--- backend/src/api/v1/routes/fuelTypes.js
+++ backend/src/api/v1/routes/fuelTypes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
-const { getFuelTypes } = require('../controllers').fuelTypes;
+const { protect, admin } = require('../middlewares');
+const { getFuelTypes, createFuelType } = require('../controllers').fuelTypes;
 
-router.get('/fuelTypes', getFuelTypes);
+router
+  .get('/fuelTypes', getFuelTypes)
+  .post('/createFuelType', protect, admin, createFuelType);
 
 module.exports = router;
