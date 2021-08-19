c98a1e2906a4ffb75eb7135a590e588c7cb8a9b3
Samdish <samdeeshsingh001@gmail.com>
2021-08-19 14:53:26 +0530

    Add route for updating fuel types and use updateFuelType controller function

--- backend/src/api/v1/routes/fuelTypes.js
+++ backend/src/api/v1/routes/fuelTypes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const { protect, admin } = require('../middlewares');
-const { getFuelTypes, createFuelType } = require('../controllers').fuelTypes;
+const { getFuelTypes, createFuelType, updateFuelType } =
+  require('../controllers').fuelTypes;
 
 router
   .get('/fuelTypes', getFuelTypes)
-  .post('/createFuelType', protect, admin, createFuelType);
+  .post('/createFuelType', protect, admin, createFuelType)
+  .put('/updateFuelType', protect, admin, updateFuelType);
 
 module.exports = router;
