edf6cea3d04b4bb65ec438de7b64f08269b1bbd4
Samdish <samdeeshsingh001@gmail.com>
2021-08-20 20:54:50 +0530

    Add route for creating and updating payment status and use their controller functions

--- backend/src/api/v1/routes/paymentStatuses.js
+++ backend/src/api/v1/routes/paymentStatuses.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const { protect, admin } = require('../middlewares');
-const { getPaymentStatuses } = require('../controllers').paymentStatuses;
+const { getPaymentStatuses, createPaymentStatus, updatePaymentStatus } =
+  require('../controllers').paymentStatuses;
 
-router.get('/paymentStatuses', protect, admin, getPaymentStatuses);
+router
+  .get('/paymentStatuses', protect, admin, getPaymentStatuses)
+  .post('/createPaymentStatus', protect, admin, createPaymentStatus)
+  .put('/updatePaymentStatus', protect, admin, updatePaymentStatus);
 
 module.exports = router;
