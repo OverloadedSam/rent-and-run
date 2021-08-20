81aa3d6f227121d47da9eab180b231320d50ce1e
Samdish <samdeeshsingh001@gmail.com>
2021-08-20 18:56:13 +0530

    Add route for updating payment method and use updatePaymentMethod controller function

--- backend/src/api/v1/routes/paymentMethods.js
+++ backend/src/api/v1/routes/paymentMethods.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const { protect, admin } = require('../middlewares');
-const { getPaymentMethods, createPaymentMethod } =
+const { getPaymentMethods, createPaymentMethod, updatePaymentMethod } =
   require('../controllers').paymentMethods;
 
 router
   .get('/paymentMethods', protect, admin, getPaymentMethods)
-  .post('/createPaymentMethod', protect, admin, createPaymentMethod);
+  .post('/createPaymentMethod', protect, admin, createPaymentMethod)
+  .put('/updatePaymentMethod', protect, admin, updatePaymentMethod);
 
 module.exports = router;
