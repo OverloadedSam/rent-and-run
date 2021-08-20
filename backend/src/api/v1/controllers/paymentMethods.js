eeafcd2f2c48d6b9b725889bb3032fdf9764265e
Samdish <samdeeshsingh001@gmail.com>
2021-08-20 18:04:33 +0530

    Payment method controller to create payment methods

--- backend/src/api/v1/controllers/paymentMethods.js
+++ backend/src/api/v1/controllers/paymentMethods.js
@@ -18,6 +18,27 @@ const getPaymentMethods = asyncHandler(async (req, res, next) => {
   });
 });
 
+// @route   POST /api/v1/createPaymentMethod
+// @access  Admin
+// @desc    Create payment methods for payments.
+const createPaymentMethod = asyncHandler(async (req, res, next) => {
+  const paymentMethodData = { ...req.body };
+
+  if (!paymentMethodData.name) {
+    return next(new ErrorResponse(400, 'Payment method name is not found!'));
+  }
+
+  const paymentMethod = new PaymentMethod(paymentMethodData);
+  const [result] = await paymentMethod.addPaymentMethod();
+
+  res.status(201).json({
+    success: true,
+    status: 201,
+    data: { ...paymentMethodData, id: result.insertId },
+  });
+});
+
 module.exports = {
   getPaymentMethods,
+  createPaymentMethod,
 };
