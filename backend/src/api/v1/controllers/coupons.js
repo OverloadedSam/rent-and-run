4705048cb0e55089ab3f14a996d83600f88c56f8
Samdish <samdeeshsingh001@gmail.com>
2021-08-21 19:35:00 +0530

    Coupon controller to create a new coupon

--- backend/src/api/v1/controllers/coupons.js
+++ backend/src/api/v1/controllers/coupons.js
@@ -1,6 +1,7 @@
 const { asyncHandler } = require('../middlewares');
 const { Coupon } = require('../models');
 const { ErrorResponse } = require('../utils');
+const { validateCouponData } = require('../validations').coupon;
 
 // @route   GET /api/v1/coupons
 // @access  Admin
@@ -18,6 +19,36 @@ const getCoupons = asyncHandler(async (req, res, next) => {
   });
 });
 
+// @route   POST /api/v1/createCoupon
+// @access  Admin
+// @desc    Create coupons to give discount on rentals.
+const createCoupon = asyncHandler(async (req, res) => {
+  const couponData = { ...req.body };
+
+  const { error } = validateCouponData(couponData);
+
+  if (error) {
+    const [validationError] = error.details;
+
+    return res.status(422).json({
+      success: false,
+      status: 422,
+      message: validationError.message,
+      key: validationError.context.key,
+    });
+  }
+
+  const coupon = new Coupon(couponData);
+  const [result] = await coupon.addCoupon();
+
+  return res.status(201).json({
+    success: true,
+    status: 201,
+    data: { ...couponData, id: result.insertId },
+  });
+});
+
 module.exports = {
   getCoupons,
+  createCoupon,
 };
