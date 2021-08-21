1c7d918308d2dad262faef30627cee9174cbef71
Samdish <samdeeshsingh001@gmail.com>
2021-08-21 21:07:00 +0530

    Add route for updating coupon data and use updateCoupon controller function

--- backend/src/api/v1/routes/coupons.js
+++ backend/src/api/v1/routes/coupons.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const { protect, admin } = require('../middlewares');
-const { getCoupons, createCoupon } =
+const { getCoupons, createCoupon, updateCoupon } =
   require('../controllers').coupons;
 
 router
   .get('/coupons', protect, admin, getCoupons)
-  .post('/createCoupon', protect, admin, createCoupon);
+  .post('/createCoupon', protect, admin, createCoupon)
+  .put('/updateCoupon/:coupon_id', protect, admin, updateCoupon);
 
 module.exports = router;
