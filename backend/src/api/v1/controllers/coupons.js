eab79573a4ea364a69eb5f1311a8f557055186b1
Samdish <samdeeshsingh001@gmail.com>
2021-08-21 21:02:45 +0530

    Coupon controller to update coupon data

--- backend/src/api/v1/controllers/coupons.js
+++ backend/src/api/v1/controllers/coupons.js
@@ -1,7 +1,8 @@
 const { asyncHandler } = require('../middlewares');
 const { Coupon } = require('../models');
 const { ErrorResponse } = require('../utils');
-const { validateCouponData } = require('../validations').coupon;
+const { validateCouponData, validateCouponUpdateData } =
+  require('../validations').coupon;
 
 // @route   GET /api/v1/coupons
 // @access  Admin
@@ -48,7 +49,53 @@ const createCoupon = asyncHandler(async (req, res) => {
   });
 });
 
+// @route   PUT /api/v1/updateCoupons
+// @access  Admin
+// @desc    Update coupon data.
+const updateCoupon = asyncHandler(async (req, res, next) => {
+  const couponData = { ...req.body };
+  const couponId = req.params.coupon_id;
+
+  const isDataEmpty = !Object.keys(couponData).length;
+  const { error } = validateCouponUpdateData(couponData);
+
+  if (isDataEmpty) {
+    return res.status(422).json({
+      success: false,
+      status: 422,
+      message: 'Please provide data to be updated',
+    });
+  }
+  if (error) {
+    const [validationError] = error.details;
+
+    return res.status(422).json({
+      success: false,
+      status: 422,
+      message: validationError.message,
+      key: validationError.context.key,
+    });
+  }
+
+  const coupon = new Coupon(couponData);
+  const [result] = await coupon.updateCoupon(couponId);
+
+  if (!result.changedRows) {
+    return next(
+      new ErrorResponse(404, 'Can not update coupon! Coupon id not found.')
+    );
+  }
+
+  return res.status(200).json({
+    success: true,
+    status: 200,
+    message: 'Coupon updated successfully',
+    data: couponData,
+  });
+});
+
 module.exports = {
   getCoupons,
   createCoupon,
+  updateCoupon,
 };
