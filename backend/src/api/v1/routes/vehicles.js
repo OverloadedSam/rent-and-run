fd102693ec30e7687bcd46556611d518823a663e
Samdish <samdeeshsingh001@gmail.com>
2021-08-23 18:47:34 +0530

    Add route for getting full details of a vehicle and use getVehicleWithDetails controller function

--- backend/src/api/v1/routes/vehicles.js
+++ backend/src/api/v1/routes/vehicles.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
-const { getVehicles } = require('../controllers').vehicles;
+const { verifyId } = require('../middlewares');
+const { getVehicles, getVehicleWithDetails } =
+  require('../controllers').vehicles;
 
 router.get('/vehicles', getVehicles);
+router
+  .route('/vehicle/:vehicle_id')
+  .get(verifyId('vehicle_id'), getVehicleWithDetails);
 
 module.exports = router;
