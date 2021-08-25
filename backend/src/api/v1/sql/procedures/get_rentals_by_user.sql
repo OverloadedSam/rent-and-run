096c587b84f38e50e15f056680de4b5188342360
Samdish <samdeeshsingh001@gmail.com>
2021-08-25 17:01:49 +0530

    Stored procedure to get rentals by user

---
+++ backend/src/api/v1/sql/procedures/get_rentals_by_user.sql
@@ -0,0 +1,28 @@
+USE rent_and_run;
+
+DROP PROCEDURE IF EXISTS get_rentals_by_user;
+
+DELIMITER $$
+CREATE PROCEDURE get_rentals_by_user(user_id CHAR(36))
+BEGIN
+  SELECT
+    r.id,
+    v.id AS vehicle_id,
+    v.brand AS vehicle_brand,
+    v.model_name AS vehicle_model_name,
+    v.images AS vehicle_images,
+    r.booking_date,
+    r.returning_date,
+    r.rent_amount,
+    ps.name AS payment_status,
+    c.discount_amount
+  FROM
+    rentals r
+    JOIN vehicles v ON r.vehicle = v.id
+    JOIN payment_statuses ps ON r.payment_status = ps.id
+    LEFT JOIN coupons c ON r.coupon = c.id
+  WHERE
+    r.user = user_id;
+END$$
+
+DELIMITER ;
