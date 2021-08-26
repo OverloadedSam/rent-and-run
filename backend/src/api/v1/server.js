9f82b267cfc38be65693879fccc87a106a228edb
Samdish <samdeeshsingh001@gmail.com>
2021-08-26 19:01:05 +0530

    Add rental router to server.js

--- backend/src/api/v1/server.js
+++ backend/src/api/v1/server.js
@@ -17,6 +17,7 @@ const apiUrl = config.get('API_URL');
 // Routes
 server.use(apiUrl, routes.userRoutes);
 server.use(apiUrl, routes.vehicleRoutes);
+server.use(apiUrl, routes.rentalRoutes);
 server.use(apiUrl, routes.roleRoutes);
 server.use(apiUrl, routes.vehicleTypeRoutes);
 server.use(apiUrl, routes.fuelTypeRoutes);
