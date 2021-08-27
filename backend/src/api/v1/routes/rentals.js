e0c8302ff6d0bc15035fab45e24b58198cef07b7
Samdish <samdeeshsingh001@gmail.com>
2021-08-27 18:35:37 +0530

    Add route for creating rentals and use createRentals controller function

--- backend/src/api/v1/routes/rentals.js
+++ backend/src/api/v1/routes/rentals.js
@@ -3,12 +3,14 @@ const { protect, verifyId } = require('../middlewares');
 const {
   getRentals,
   getUserRentals,
-  getRentalById
+  getRentalById,
+  createRental,
 } = require('../controllers').rentals;
 
 router
   .get('/rentals', protect, getRentals)
   .get('/myRentals', protect, getUserRentals)
-  .get('/rental/:rental_id', verifyId('rental_id'), protect, getRentalById);
+  .get('/rental/:rental_id', verifyId('rental_id'), protect, getRentalById)
+  .post('/createRental/:vehicle_id', verifyId('vehicle_id'), protect, createRental);
 
 module.exports = router;
