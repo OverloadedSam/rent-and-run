e6d5615301678d47c7bfc7a37f0d5a7f5ead1771
Samdish <samdeeshsingh001@gmail.com>
2021-08-27 20:03:19 +0530

    Add route for applying or updating coupon for a rental and use applyCoupon controller function

--- backend/src/api/v1/routes/rentals.js
+++ backend/src/api/v1/routes/rentals.js
@@ -5,12 +5,14 @@ const {
   getUserRentals,
   getRentalById,
   createRental,
+  applyCoupon,
 } = require('../controllers').rentals;
 
 router
   .get('/rentals', protect, getRentals)
   .get('/myRentals', protect, getUserRentals)
   .get('/rental/:rental_id', verifyId('rental_id'), protect, getRentalById)
+  .put('/applyCoupon/:rental_id', protect, verifyId('rental_id'), applyCoupon)
   .post('/createRental/:vehicle_id', verifyId('vehicle_id'), protect, createRental);
 
 module.exports = router;
