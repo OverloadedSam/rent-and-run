0c9fb8f53193bd70ada06dc980783008a1914885
Samdeesh <samdeeshsingh001@gmail.com>
2021-09-09 18:21:11 +0530

    Build loader common component

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -4,5 +4,6 @@ import Button from './Button';
 import Card from './Card';
 import Grid from './Grid';
 import Collapsible from './Collapsible';
+import Loader from './Loader';
 
-export { Container, Badge, Button, Card, Grid, Collapsible };
+export { Container, Badge, Button, Card, Grid, Collapsible, Loader };
