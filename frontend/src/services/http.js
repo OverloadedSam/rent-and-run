090f597f0749d697e1e327fe7ca70432a9ffa490
Samdeesh <samdeeshsingh001@gmail.com>
2021-09-13 20:20:19 +0530

    Define http service

---
+++ frontend/src/services/http.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
+const http = {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete,
+};
+
+export default http;
