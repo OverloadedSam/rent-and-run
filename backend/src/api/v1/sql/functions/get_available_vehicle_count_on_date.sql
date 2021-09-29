29c3cab168097e09e5c4d472e3850b5fe1c0d87f
Samdeesh <samdeeshsingh001@gmail.com>
2021-09-29 20:43:49 +0530

    Function to get available count for a vehicle on given date range

---
+++ backend/src/api/v1/sql/functions/get_available_vehicle_count_on_date.sql
@@ -0,0 +1,91 @@
+USE rent_and_run;
+
+DROP FUNCTION IF EXISTS get_available_vehicle_count_on_date;
+
+DELIMITER $ $ CREATE FUNCTION get_available_vehicle_count_on_date (
+  vehicle_id VARCHAR(36),
+  current_booking_date DATETIME,
+  current_returning_date DATETIME
+) RETURNS INTEGER READS SQL DATA BEGIN DECLARE unreserved_in_rentals INT DEFAULT 0;
+
+DECLARE available_in_vehicles INT DEFAULT 0;
+
+DECLARE total_unreserved_vehicles INT DEFAULT 0;
+
+SELECT
+  COUNT(*) INTO unreserved_in_rentals
+FROM
+  rentals r
+WHERE
+  (
+    r.returning_date > NOW()
+    AND r.returning_date < current_booking_date
+    AND r.payment_status = 1
+    AND r.vehicle = vehicle_id
+  )
+  OR (
+    r.booking_date > NOW()
+    AND r.booking_date > current_returning_date
+    AND r.payment_status = 1
+    AND r.vehicle = vehicle_id
+  );
+
+SELECT
+  available_count INTO available_in_vehicles
+FROM
+  vehicles v
+WHERE
+  v.id = vehicle_id;
+
+SET
+  total_unreserved_vehicles = unreserved_in_rentals + available_in_vehicles;
+
+RETURN total_unreserved_vehicles;
+
+END $ $ DELIMITER;
+
+USE rent_and_run;
+
+DROP FUNCTION IF EXISTS get_available_vehicle_count_on_date;
+
+DELIMITER $ $ CREATE FUNCTION get_available_vehicle_count_on_date (
+  vehicle_id VARCHAR(36),
+  current_booking_date DATETIME,
+  current_returning_date DATETIME
+) RETURNS INTEGER READS SQL DATA BEGIN DECLARE unreserved_in_rentals INT DEFAULT 0;
+
+DECLARE available_in_vehicles INT DEFAULT 0;
+
+DECLARE total_unreserved_vehicles INT DEFAULT 0;
+
+SELECT
+  COUNT(*) INTO unreserved_in_rentals
+FROM
+  rentals r
+WHERE
+  (
+    r.returning_date > NOW()
+    AND r.returning_date < current_booking_date
+    AND r.payment_status = 1
+    AND r.vehicle = vehicle_id
+  )
+  OR (
+    r.booking_date > NOW()
+    AND r.booking_date > current_returning_date
+    AND r.payment_status = 1
+    AND r.vehicle = vehicle_id
+  );
+
+SELECT
+  available_count INTO available_in_vehicles
+FROM
+  vehicles v
+WHERE
+  v.id = vehicle_id;
+
+SET
+  total_unreserved_vehicles = unreserved_in_rentals + available_in_vehicles;
+
+RETURN total_unreserved_vehicles;
+
+END $ $ DELIMITER;
