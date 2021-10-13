d5a50e035cd17d9c90247055f92301efc9491102
Samdeesh <samdeeshsingh001@gmail.com>
2021-10-13 20:00:13 +0530

    Attach auth token by default in every http request

--- frontend/src/services/http.js
+++ frontend/src/services/http.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import auth from './authService';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.headers.common = { Authorization: `Bearer ${auth.getAuthToken()}` };
 
 const http = {
   get: axios.get,
